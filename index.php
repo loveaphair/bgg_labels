f54dfa4ff10a90531095bad87a63cdee74d3e9f2
loveaphair <37857853+loveaphair@users.noreply.github.com>
2021-07-29 12:30:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	</head>
 	<style>
 		.collection {
@@ -25,7 +26,7 @@
 		.set-min {
 			min-height:130px;
 		}
-		.container {
+		.my-container {
 			max-width:600px;
 		}
 	</style>
@@ -37,45 +38,49 @@
 		});
 	</script>
 	<body>
-	<div class="container">
+	<div class="container-fluid">
 
 <?php
 // Show the form to get their data
 if(empty($_GET['user_name'])) { ?>
-		<div class="row mt-4">
-			<div class="col">
-				<h2>Board Game Label Generator</h2>
-				<h4>Instructions:</h4>
-				<p>Go to your <a href="https://boardgamegeek.com" target="_blank">boardgamegeek.com</a> account and view your <strong>collection</strong>.</p>
-				<p>Add all the games you want to sell and update the status to include <strong>For Trade</strong>. <br>
-				In the comments include, just the numerical value you wish to sell the game for.<br>
-				If you switch the view of your collection to just <strong>For Trade</strong>, you can supply condition information also.</p>
-				<p>After you've updated your collection, return here and supply the following information:</p>
-			</div>
-		</div>
 		<div class="row">
-			<div class="col">
-				<form id="getUserInfo" action="">
-					<div class="form-floating mb-3">
-						<input type="text" name="user_name" id="user_name" class="form-control" placeholder="bggusername">
-						<label for="user_name">BoardGameGeek Username</label>
+			<div class="col-12 offset-0 offset-lg-4 col-lg-4">
+				<div class="row mt-4">
+					<div class="col">
+						<h2>Board Game Label Generator</h2>
+						<h4>Instructions:</h4>
+						<p>Go to your <a href="https://boardgamegeek.com" target="_blank">boardgamegeek.com</a> account and view your <strong>collection</strong>.</p>
+						<p>Add all the games you want to sell and update the status to include <strong>For Trade</strong>. <br>
+						In the comments include, just the numerical value you wish to sell the game for.<br>
+						If you switch the view of your collection to just <strong>For Trade</strong>, you can supply condition information also.</p>
+						<p>After you've updated your collection, return here and supply the following information:</p>
 					</div>
-					<div class="form-floating mb-3">
-						<input type="text" name="venmo_user" id="venmo_user" class="form-control" placeholder="my-venmo-username-c9965">
-						<label for="venmo_user">Venmo Username</label>
-						<div id="venmo_help" class="form-text">Example: my-venmo-username-c9965</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<form id="getUserInfo" action="">
+							<div class="form-floating mb-3">
+								<input type="text" name="user_name" id="user_name" class="form-control" placeholder="bggusername">
+								<label for="user_name">BoardGameGeek Username</label>
+							</div>
+							<div class="form-floating mb-3">
+								<input type="text" name="venmo_user" id="venmo_user" class="form-control" placeholder="my-venmo-username-c9965">
+								<label for="venmo_user">Venmo Username</label>
+								<div id="venmo_help" class="form-text">Example: my-venmo-username-c9965</div>
+							</div>
+							<div class="form-floating mb-3">
+								<input type="text" name="seller_name" class="form-control" id="phone_number" placeholder="Your Name">
+								<label for="seller_name">Your Name</label>
+							</div>
+							<div class="form-floating mb-3">
+								<input type="text" name="phone_number" class="form-control" id="phone_number" placeholder="Mobile Number">
+								<label for="phone_number">Your Cell Number</label>
+								<div id="cell_help" class="form-text">Format the number the way you want it shown ( i.e. (801) 555-5555 )</div>
+							</div>
+							<button type="submit" class="btn btn-primary">Submit</button>
+						</form>
 					</div>
-					<div class="form-floating mb-3">
-						<input type="text" name="seller_name" class="form-control" id="phone_number" placeholder="Your Name">
-						<label for="seller_name">Your Name</label>
-					</div>
-					<div class="form-floating mb-3">
-						<input type="text" name="phone_number" class="form-control" id="phone_number" placeholder="Mobile Number">
-						<label for="phone_number">Your Cell Number</label>
-						<div id="cell_help" class="form-text">Format the number the way you want it shown ( i.e. (801) 555-5555 )</div>
-					</div>
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</form>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -144,47 +149,49 @@ $row_class = empty($qr_code) ? 'set-min' : '';
 $second_col_class = !empty($qr_code) ? 'border-end-0' : '';
 foreach($games as $game) { 
 	if($i === 1) {
-		echo '<div class="collection">';
+		echo '<div class="collection row">';
 	}?>
-		<div class="row mb-1 <?=$row_class?>">
-			<div class="img-col col border border-dark position-relative">
-				<div class="position-absolute top-50 start-50 translate-middle h-100">
-					<img src="<?=$game['image'] ?>" class="h-100">
+		<div class="col-10 offset-1 col-lg-4 offset-lg-4">
+			<div class="row mb-1 <?=$row_class?>">
+				<div class="img-col col border border-dark position-relative">
+					<div class="position-absolute top-50 start-50 translate-middle h-100">
+						<img src="<?=$game['image'] ?>" class="h-100">
+					</div>
 				</div>
+				<div class="col border border-dark border-start-0 border-end-0 position-relative">
+					<div class="row justify-content-between mt-2">
+						<div class="col col-auto">
+							<h5 class="d-inline-block text-truncate" style="max-width:300px;"><?=$game['title']?></h5>
+						</div>
+						<div class="col-2 col text-end">
+							<h5><?=$game['price']?></h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col seller-info <?=$second_col_class?> col-6">
+							<p><b>Seller Name:</b> <?=$_GET['seller_name'] ?? ''?></p>
+							<p><b>Cell Number:</b><?=$_GET['phone_number'] ?? ''?></p>
+						</div>
+						<div class="col seller-info  <?=$second_col_class?> col-6">
+							<p><b>Year:</b> <?=$game['published']?> <strong>|</strong> <b>BGG Rating:</b> <?=$game['rating']?></p>
+							<p><?=$game['min_players']?> - <?=$game['max_players']?> Players, <?=$game['play_time']?> Minutes</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col col-12">
+						<?php if(!empty($game['condition'])) { ?>
+								<p><b>Condition:</b> <?=$game['condition']?></p>
+							<?php } ?>
+						</div>
+					</div>
+				</div>
+			<?php if(!empty($qr_code)) { ?>
+				<div class="img-col col border border-dark mx-auto d-block text-center">
+					<h5>Venmo</h5>
+					<img class="venmo-img" src="<?=$qr_code?>">
+				</div>
+			<?php } ?>
 			</div>
-			<div class="col border border-dark border-start-0 border-end-0 position-relative">
-				<div class="row justify-content-between mt-2">
-					<div class="col col-auto">
-						<h5 class="d-inline-block text-truncate" style="max-width:300px;"><?=$game['title']?></h5>
-					</div>
-					<div class="col-2 col text-end">
-						<h5><?=$game['price']?></h5>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col seller-info <?=$second_col_class?> col-6">
-						<p><b>Seller Name:</b> <?=$_GET['seller_name'] ?? ''?></p>
-						<p><b>Cell Number:</b><?=$_GET['phone_number'] ?? ''?></p>
-					</div>
-					<div class="col seller-info  <?=$second_col_class?> col-6">
-						<p><b>Year:</b> <?=$game['published']?> <strong>|</strong> <b>BGG Rating:</b> <?=$game['rating']?></p>
-						<p><?=$game['min_players']?> - <?=$game['max_players']?> Players, <?=$game['play_time']?> Minutes</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col col-12">
-					<?php if(!empty($game['condition'])) { ?>
-							<p><b>Condition:</b> <?=$game['condition']?></p>
-						<?php } ?>
-					</div>
-				</div>
-			</div>
-		<?php if(!empty($qr_code)) { ?>
-			<div class="img-col col border border-dark mx-auto d-block text-center">
-				<h5>Venmo</h5>
-				<img class="venmo-img" src="<?=$qr_code?>">
-			</div>
-		<?php } ?>
 		</div>
 <?php 
 	if($i === 7) {
