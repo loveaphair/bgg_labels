030d4b299e5404098254747f0fe4e361f9b34b4a
loveaphair <37857853+loveaphair@users.noreply.github.com>
2022-02-28 22:20:21 -0700

    Update index.php
    changed the css layout. added support for using venmo qr code image and converting from google drive.

--- index.php
+++ index.php
@@ -10,14 +10,16 @@
 			page-break-after:always;
 			font-size:75%;
 		}
-		img {
-			max-width:90px;
-		}
 		.venmo-img {
-			max-width:70px;
+			max-width:100px;
 		}
 		.img-col {
-			max-width:90px;
+			max-width:100px;
+			display: flex;
+			align-content: center;
+			justify-content: center;
+			flex-direction: column;
+}
 		}
 		.seller-info p {
 			line-height:12px;
@@ -29,11 +31,29 @@
 		.my-container {
 			max-width:600px;
 		}
+		.mh150 {
+			max-width:100% !important;
+			height:auto;
+		}
 	</style>
 	<script type="text/javascript">
 		$(function() {
 			$('#reload').on('click', function() {
 				location.reload();
+			});
+  
+			var url = $( '#venmo_img' ),
+				btn = $( '#venmo_btn' );
+
+			btn.on( 'click', function( event ) {
+				event.preventDefault();
+				found = url.val().match( /d\/([A-Za-z0-9\-]+)/ );
+				
+				if ( found[1].length ) {
+				new_url = 'https://drive.google.com/uc?export=view&id=' + found[1];
+				
+				url.val( new_url );
+				}
 			});
 		});
 	</script>
@@ -64,9 +84,17 @@ if(empty($_GET['user_name'])) { ?>
 								<label for="user_name">BoardGameGeek Username</label>
 							</div>
 							<div class="form-floating mb-3">
-								<input type="text" name="venmo_user" id="venmo_user" class="form-control" placeholder="my-venmo-username-c9965">
+								<input type="text" name="venmo_user" id="venmo_user" class="form-control" placeholder="@my-venmo-username-c9965">
 								<label for="venmo_user">Venmo Username</label>
 								<div id="venmo_help" class="form-text">Example: my-venmo-username-c9965</div>
+							</div>
+							<div class="form-floating mb-3">
+								<input type="text" name="venmo_img" id="venmo_img" class="form-control" placeholder="https://some-image-sharing-site.com/your-image.jpg">
+								<label for="venmo_user">Link to Venmo QR Code</label>
+								<div id="venmo_help" class="form-text">
+									<span>If you're using a link from google drive, use this button to convert it to a useable image.</span>
+									<button type"button" id="venmo_btn" class="btn btn-secondary">Convert Google Drive URL</button>
+								</div>
 							</div>
 							<div class="form-floating mb-3">
 								<input type="text" name="seller_name" class="form-control" id="phone_number" placeholder="Your Name">
@@ -90,12 +118,6 @@ if(empty($_GET['user_name'])) { ?>
 } // No username
 
 $username = $_GET['user_name'];
-$qr_code = '';
-if(!empty($_GET['venmo_user'])) {
-	$string = trim(str_replace('@', '', $_GET['venmo_user']));
-	$venmo_link = 'https://venmo.com/'.$string;
-	$qr_code = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.urlencode($venmo_link);
-}
 
 $url = "https://boardgamegeek.com/xmlapi2/collection?username={$username}&trade=1&stats=1";
 
@@ -110,7 +132,7 @@ curl_close($ch);
 
 //convert the XML result into array
 $array_data = json_decode(json_encode(simplexml_load_string($data)), true);
-if(!is_array($array_data['item'])) {
+if(empty($array_data['item']) && !is_array($array_data['item'])) {
 	echo $array_data[0];
 	echo '<br>';
 	echo '<button id="reload" class="btn btn-primary">Reload</button>';
@@ -123,8 +145,8 @@ foreach($array_data['item'] as $key => $game) {
 	if(empty($game['status']['@attributes']['fortrade'])) {
 		continue;
 	}
-	$price = trim(preg_replace("/[^0-9]/", "", $game['comment']));
-	$text = trim(preg_replace("/[^a-zA-Z]/", " ", $game['comment']));
+	$price = trim(preg_replace("/[^0-9]/", "", ($game['comment'] ?? '')));
+	$text = trim(preg_replace("/[^a-zA-Z]/", " ", ($game['comment'] ?? '')));
 	$condition = !empty($game['conditiontext']) ? $game['conditiontext'].$text : ($text ?: '');
 	$games[] = [
 		'title' => $game['name'] ?? 'Unkown Game',
@@ -136,7 +158,7 @@ foreach($array_data['item'] as $key => $game) {
 		'rating' => number_format($game['stats']['rating']['average']['@attributes']['value'], 1),
 		'min_players' => $game['stats']['@attributes']['minplayers'],
 		'max_players' => $game['stats']['@attributes']['maxplayers'],
-		'play_time' => $game['stats']['@attributes']['playingtime'],
+		'play_time' => $game['stats']['@attributes']['playingtime'] ?? 0,
 	];
 }
 
@@ -149,48 +171,43 @@ $row_class = empty($qr_code) ? 'set-min' : '';
 $second_col_class = !empty($qr_code) ? 'border-end-0' : '';
 foreach($games as $game) { 
 	if($i === 1) {
-		echo '<div class="collection row">';
+		echo '<div class="collection row d-flex flex-column justify-content-center align-items-center">';
 	}?>
-		<div class="col-10 offset-1 col-lg-4 offset-lg-4">
-			<div class="row mb-1 <?=$row_class?>">
-				<div class="img-col col border border-dark position-relative">
-					<div class="position-absolute top-50 start-50 translate-middle h-100">
-						<img src="<?=$game['image'] ?>" class="h-100">
-					</div>
+		<div class="col-10 m-1" style="border:1px solid black;max-height:120px;">
+			<div class="row">
+				<div class="col-2 d-flex justify-content-center align-items-center position-relative" style="border-right:1px solid black">
+					<img src="<?=$game['image'] ?>" class="position-absolute" style="max-height:100%;max-width:100%;">
 				</div>
-				<div class="col border border-dark border-start-0 border-end-0 position-relative">
-					<div class="row justify-content-between mt-2">
-						<div class="col col-auto">
-							<h5 class="d-inline-block text-truncate" style="max-width:300px;"><?=$game['title']?></h5>
+				<div class="col-8">
+					<div class="row">
+						<div class="col">
+							<h5 class="d-inline-block text-truncate" style="max-width:350px;"><?=$game['title']?></h5>
 						</div>
-						<div class="col-2 col text-end">
+					</div>
+					<div class="row">
+						<div class="col">
 							<h5><?=$game['price']?></h5>
+							<b>Year:</b> <?=$game['published']?> <strong>|</strong> <b>BGG Rating:</b> <?=$game['rating']?>
+							<?php if(!empty($game['condition'])): ?>
+								<br>
+								<b>Condition:</b> <?=$game['condition']?>
+							<?php endif; ?>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col seller-info <?=$second_col_class?> col-6">
-							<p><b>Seller Name:</b> <?=$_GET['seller_name'] ?? ''?></p>
-							<p><b>Cell Number:</b><?=$_GET['phone_number'] ?? ''?></p>
-						</div>
-						<div class="col seller-info  <?=$second_col_class?> col-6">
-							<p><b>Year:</b> <?=$game['published']?> <strong>|</strong> <b>BGG Rating:</b> <?=$game['rating']?></p>
-							<p><?=$game['min_players']?> - <?=$game['max_players']?> Players, <?=$game['play_time']?> Minutes</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col col-12">
-						<?php if(!empty($game['condition'])) { ?>
-								<p><b>Condition:</b> <?=$game['condition']?></p>
-							<?php } ?>
+						<div class="col">
+							<b>Seller Name:</b> <?=$_GET['seller_name'] ?? ''?><br>
+							<b>Cell Number:</b><?=$_GET['phone_number'] ?? ''?><br>
+							<?=$game['min_players']?> - <?=$game['max_players']?> Players, <?=$game['play_time']?> Minutes
 						</div>
 					</div>
 				</div>
-			<?php if(!empty($qr_code)) { ?>
-				<div class="img-col col border border-dark mx-auto d-block text-center">
-					<h5>Venmo</h5>
-					<img class="venmo-img" src="<?=$qr_code?>">
+				<div class="col-2 d-flex justify-content-center align-items-center flex-column" style="border-left:1px solid black">
+					<?php if(!empty($_GET['venmo_img'])) { ?>
+					<!-- <div class="img-col col border border-dark mx-auto text-center"> -->
+						<img class="venmo-img" src="<?=$_GET['venmo_img']?>" width="100">
+						<span><?=$_GET['venmo_user']?></span>
+					<!-- </div> -->
+					<?php } ?>
 				</div>
-			<?php } ?>
 			</div>
 		</div>
 <?php 
